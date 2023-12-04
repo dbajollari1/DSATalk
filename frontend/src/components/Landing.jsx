d460dae83d9bd3d4fb4347537a1dabae7f405245
Maheshdh <mahesh.dhondge@gmail.com>
2023-12-04 16:23:53 -0500

    Rendering questions component insiede home page

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Questions from './Questions';
 import '../App.css';
 
 function Landing() {
   return (
     <div className='card'>
-      <h2>This is the Landing page</h2>
+      <h1> Welcome to DSAtalk!</h1>
+      <p> list of blind 75 questions </p>
+      <Questions/>
     </div>
   );
 }
