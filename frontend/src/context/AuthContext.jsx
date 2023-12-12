ecf22ac22a5a60e27b9f64ae9b6c4ebcec7843e7
Maheshdh <mahesh.dhondge@gmail.com>
2023-12-12 00:56:53 -0500

    Storing user to backend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {getAuth, onAuthStateChanged} from 'firebase/auth';
+import axios from 'axios';
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({children}) => {
@@ -8,7 +9,10 @@ export const AuthProvider = ({children}) => {
   const auth = getAuth();
   useEffect(() => {
     let myListener = onAuthStateChanged(auth, (user) => {
+      if(user){
       setCurrentUser(user);
+      saveUserToBackend(user);
+      }
       console.log('onAuthStateChanged', user);
       setLoadingUser(false);
     });
@@ -16,6 +20,21 @@ export const AuthProvider = ({children}) => {
       if (myListener) myListener();
     };
   }, []);
+
+  // function to make a backend request to save current user to mongodb
+  const saveUserToBackend = async (user) => {
+    try {
+      const response = await axios.post('http://localhost:3000/users/addUser', {
+        email: user.email,
+        username: user.displayName,
+        // Add other user data as needed
+      });
+
+      } catch (error) {
+      // Handle errors
+      console.error('Error saving user to backend:', error.message);
+    }
+  };
 
   if (loadingUser) {
     return (
