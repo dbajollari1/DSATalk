0f2610db83056830bc1973c8b130b11e1d176d7d
David Bajollari <db@davids-mbp.mynetworksettings.com>
2023-12-13 15:37:42 -0500

    add firebase auth in backend

--- frontend/src/components/Discussion.jsx
+++ frontend/src/components/Discussion.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext} from 'react';
+import {AuthContext} from '../context/AuthContext';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function Discussion() {
     const [discussion, setDiscussion] = useState([]);
+    const {currentUser} = useContext(AuthContext);
+    console.log(currentUser.accessToken)
     const { id } = useParams();
 
     useEffect(() => {
         const fetchDiscussion = async () => {
-            const res = await axios.get('http://localhost:3000/discussions/discussion/' + id);
+            const headers = {
+                'Authorization': `Bearer ${currentUser.accessToken}`, 
+                'Content-Type': 'application/json',
+            };
+            const res = await axios.get('http://localhost:3000/discussions/discussion/' + id, {headers});
             setDiscussion(res.data);
         }
         fetchDiscussion();
